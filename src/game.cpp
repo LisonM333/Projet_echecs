89b8af6884bab6cf31becad8ece87f45fb383f04
Oceane Drapeau <ocedrapeau@gmail.com>
2026-01-31 15:14:29 +0100

    ✨ [game] add class Game

--- src/game.cpp
+++ src/game.cpp
@@ -1,1 +1,25 @@
 #include "./includes/game.hpp"
+#include "includes/board.hpp"
+#include "includes/player.hpp"
+
+// Constructors
+Game::~Game() = default;
+
+Game::Game() : m_player(Player()), m_white_playing(true), m_board(Board()) {};
+Game::Game(const Game& game) : m_player(game.m_player), m_white_playing(game.m_white_playing), m_board(game.m_board) {};
+
+Game& Game::operator=(const Game& game)
+{
+    if (this != &game)
+    {
+        m_player        = game.m_player;
+        m_white_playing = game.m_white_playing;
+        m_board         = game.m_board;
+    }
+    return *this;
+}
+
+// Methods
+
+void Game::start() {};
+void Game::end() {};
--- src/includes/game.hpp
+++ src/includes/game.hpp
@@ -1,1 +1,22 @@
 #pragma once
+#include "board.hpp"
+#include "player.hpp"
+
+class Game {
+private:
+    Player m_player;
+    bool   m_white_playing;
+    Board  m_board;
+
+public:
+    ~Game();
+
+    Game();
+    Game(const Game& game);
+
+    Game& operator=(const Game& game);
+    void  get_board();
+
+    void start();
+    void end();
+};
