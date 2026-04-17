00cdb899b3f0cfc1af92dc2298f9ae8d63467a6e
Oceane Drapeau <ocedrapeau@gmail.com>
2026-04-17 08:29:03 +0200

    🚧 [board2DRender.hpp] trying to add 2D logic into 3D

---
+++ src/includes/board2DRenderer.hpp
@@ -0,0 +1,12 @@
+// #pragma once
+// #include "board.hpp"
+
+// const char* get_label(const int& line, const int& colum);
+
+// template<typename T, typename Q>
+// // bool is_in(const T& value, const std::vector<Q>& list); // T and Q are different only for the case of the comparaison between char* and string
+
+// bool square_colored(const std::vector<Position>& squares, const Position& square);
+// void square_representation(const int& line, const int& colum, const char*& label, const ImVec4& colo_case, const ImVec4& colo_piece, bool& selected); // responsabilise the apparence of one square
+
+// void board2DRendererImGui(Board const& board);
